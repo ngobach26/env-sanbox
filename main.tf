a3e205556e63b148b33193b7a3444c2ac6e47c4f
ngobach26 <ngobach26@gmail.com>
2025-09-02 05:37:44 +0700

    add network submodule

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket  = "teraform-state-mur" # đảm bảo bucket này đã tồn tại
     key     = "teraform/backend"   # đường dẫn file state trong bucket
-    region  = "us-east-1"           # phải trùng region của bucket
-    encrypt = true                  # khuyến nghị: mã hóa state
+    region  = "us-east-1"          # phải trùng region của bucket
+    encrypt = true                 # khuyến nghị: mã hóa state
   }
 }
 
@@ -12,3 +12,21 @@ locals {
   aws_region       = "us-east-1"
   k8s_cluster_name = "ms-cluster"
 }
+
+# Network Configuration
+module "aws-network" {
+  source                = "github.com/ngobach26/module-aws-network"
+  env_name              = local.env_name
+  vpc_name              = "msur-VPC"
+  cluster_name          = local.k8s_cluster_name
+  aws_region            = local.aws_region
+  main_vpc_cidr         = "10.10.0.0/16"
+  public_subnet_a_cidr  = "10.10.0.0/18"
+  public_subnet_b_cidr  = "10.10.64.0/18"
+  private_subnet_a_cidr = "10.10.128.0/18"
+  private_subnet_b_cidr = "10.10.192.0/18"
+}
+
+# EKS Configuration
+
+# GitOps Configuration
